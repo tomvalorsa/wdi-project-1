96d1147020a7c181155d6b73f1b4f367977f680c
Tom Valorsa <tomvalorsa@gmail.com>
2015-02-08 15:18:02 +1100

    create and view individual posts + index page

--- code_stock_app/app/controllers/posts_controller.rb
+++ code_stock_app/app/controllers/posts_controller.rb
@@ -1,11 +1,23 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find params[:id]
   end
 
   def create
+    @post = Post.create post_params
+
+    if @post.save
+      @post.user_id = @current_user.id
+      redirect_to "/posts/#{@post.id}"
+    end
   end
 
   def edit
@@ -15,5 +27,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :codepen, :description)
   end
 end
