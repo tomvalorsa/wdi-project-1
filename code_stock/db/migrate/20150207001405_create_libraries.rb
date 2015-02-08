4d03511046ea78a697a5385e19a5fc31138edd08
Tom Valorsa <tomvalorsa@gmail.com>
2015-02-08 13:34:51 +1100

    getting deleted files off index/learning stuff

--- code_stock/db/migrate/20150207001405_create_libraries.rb
+++
@@ -1,8 +0,0 @@
-class CreateLibraries < ActiveRecord::Migration
-  def change
-    create_table :libraries do |t|
-      t.integer :user_id
-      t.timestamps
-    end
-  end
-end
--- code_stock/db/migrate/20150207001545_create_libraries_posts.rb
+++
@@ -1,8 +0,0 @@
-class CreateLibrariesPosts < ActiveRecord::Migration
-  def change
-    create_table :libraries_posts do |t|
-      t.integer :library_id
-      t.integer :post_id
-    end
-  end
-end
