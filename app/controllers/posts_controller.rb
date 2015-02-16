689c29ec5d57fd0d2adcf090fb454d26c93c73e0
Tom Valorsa <tomvalorsa@gmail.com>
2015-02-16 15:32:42 +1100

    fixed horrible url sub

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,17 +27,7 @@ class PostsController < ApplicationController
       @post.update(:user_id => @current_user.id)
       @current_user.library.posts << @post
 
-      # url_sub = @post.codepen.gsub("pen", "embed")
-
-      # literally the worst thing ever. Make it cleaner and less horrific by learning how to gsub out the last occurrence of a substring (attempt at gsub above needs tweaking)
-      url_array = @post.codepen.split('/')
-      url_array[0] = 'https:/'
-      url_array[1] += '/'
-      url_array[2] += '/'
-      url_array[3] += '/'
-      url_array[4] = 'embed/'
-      url_array[5] += '/'
-      x = url_array.join
+      x = @post.codepen.gsub("/pen", "/embed").gsub("http", "https")
 
       @post.update(:codepen_embed => x)
       redirect_to "/library/posts/#{ @post.id }"
